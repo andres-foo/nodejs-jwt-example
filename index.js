3be28826f6d7c92bc64fbdff67ce7311fa3ae819
andres-foo <jvillanelo@gmail.com>
2020-06-06 21:14:39 -0400

    Implement jwt with inline verification of token

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express()
 
 const router = express.Router()
 
+// constants
+const PORT = process.env.PORT || 3000
+const HOST = process.env.HOST || '127.0.0.1'
+const SECRET_KEY = process.env.SECRET_KEY || 'secretkey'
+
 // regular route
 router.get('/', (req, res) => {
   res.send('Home')
@@ -12,12 +17,35 @@ router.get('/', (req, res) => {
 
 // login
 router.post('/login', (req, res) => {
-  res.json(req.body)
+  // TODO: verify user exists
+
+  // payload with user data
+  const payload = {
+    userid: 1,
+    username: req.body.username
+  }
+  // create token
+  jwt.sign(payload, SECRET_KEY, (err, token) => {
+    if(err) {
+      res.json({error: 'Couldn\'t generate a key'})
+    }
+    res.json({token})
+  })  
 })
 
-// protected
+// protected route
 router.post('/protected', (req, res) => {
-  res.send('Protected data')
+  // get token
+  const authorization = req.headers['authorization']
+  const token = authorization.split(' ')[1]
+
+  // verify token
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if(err) {
+      res.json({error: 'Not a valid token'})
+    }
+    res.json({decoded, data: 'Protected data'})
+  }) 
 })
 
 // parse requests of content-type - application/json
@@ -27,8 +55,5 @@ app.use(express.urlencoded({extended: false}))
 // routes at the end to allow previous middleware to act
 app.use(router)
 
-// start
-const PORT = process.env.PORT || 3000
-const HOST = process.env.HOST || '127.0.0.1' 
-
+//start
 app.listen(3000, () => console.log(`Listening on http://${HOST}:${PORT}`))
