bed73be77d5b2cd7c43c3a8c8fa453fff9ec5170
andres-foo <jvillanelo@gmail.com>
2020-06-06 20:42:41 -0400

    Create login route and protected route

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 require('dotenv').config()
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const app = express()
 
 const router = express.Router()
 
+// regular route
 router.get('/', (req, res) => {
-  res.send('Working')
+  res.send('Home')
 })
 
+// login
+router.post('/login', (req, res) => {
+  res.json(req.body)
+})
+
+// protected
+router.post('/protected', (req, res) => {
+  res.send('Protected data')
+})
+
+// parse requests of content-type - application/json
+app.use(express.json())
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: false}))
+// routes at the end to allow previous middleware to act
 app.use(router)
 
+// start
 const PORT = process.env.PORT || 3000
 const HOST = process.env.HOST || '127.0.0.1' 
 
